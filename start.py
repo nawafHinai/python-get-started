aa577d7ab023b36b6999e19f70037183849ee58f
unknown <hinainawaf@gmail.com>
2019-10-09 17:23:59 +0400

    just starting

---
+++ start.py
@@ -0,0 +1,17 @@
+name = "nawaf"
+print(name, type(name))
+
+integer = 964583
+print(integer, type(integer))
+
+pi = 3.14
+print(pi, type(pi))
+
+isHere = True
+print(isHere, type(isHere))
+
+null = None
+print(null, type(null))
+
+a, b, c = 1, 2, 3
+print(a,b,c)
